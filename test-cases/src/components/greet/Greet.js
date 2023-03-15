4466703c275bf569a471c676cb1c14294033e850
nareshb7 <nareshbjava7@gmail.com>
2023-03-15 11:46:49 +0530

    first

--- test-cases/src/components/greet/Greet.js
+++ test-cases/src/components/greet/Greet.js
@@ -1,11 +1,18 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 const Greet = ({name}) => {
   const [isHide, setIsHide] = useState(true)
+  const skills = ['HTML', 'CSS', 'JavaScript']
+  const [time, setTime] = useState(false)
   const handleClick =()=> {
     setIsHide(false)
     return 'Ok'
   }
+  useEffect(()=> {
+    setTimeout(()=> {
+      setTime(true)
+    }, 1200)
+  })
   return (
     <div>
       <h1>Hello {name}</h1>
@@ -13,7 +20,16 @@ const Greet = ({name}) => {
       {
         isHide && <h2 id ='demo'>Dismiss</h2>
       }
-      
+      <input type='text' name='Location'/>
+      <ul>
+        {
+          skills.map(skill => <li>{skill}</li>)
+        }
+      </ul>
+       <img src='' alt='image' />
+        {
+          time && <h3>Timeout Check</h3>
+        } 
     </div>
   )
 }
--- test-cases/src/components/greet/Greet.test.js
+++ test-cases/src/components/greet/Greet.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, logRoles } from "@testing-library/react";
 import Greet from "./Greet";
 
 describe('Greet Component', ()=> {
@@ -10,13 +10,41 @@ describe('Greet Component', ()=> {
     
     test('Checking greet message with name', ()=> {
         render(<Greet name={'Naresh'} />)
-        const el = screen.getByText('Hello Naresh')
+        const el = screen.getByRole('heading', {name: 'Hello Naresh' })
         expect(el).toBeInTheDocument()
+    })
+    test('Input Filed is present or not', ()=> {
+        render(<Greet />)
+        const inputElment = screen.getByRole('textbox')
+        expect(inputElment).toBeInTheDocument()
     })
     test('Check Dismiss is Present', ()=> {
         render(<Greet/>)
         const text = screen.getByText('Dismiss')
         expect(text).toBeInTheDocument()
+    })
+    test('UL is present or not', ()=> {
+        render(<Greet/>)
+        const ul = screen.getByRole('list')
+        expect(ul).toBeInTheDocument()
+        const listItems = screen.getAllByRole('listitem')
+        logRoles(ul)
+        screen.debug(listItems)
+        expect(listItems).toHaveLength(3)
+    })
+    test.only('Timeout header is render or not',async ()=> {
+        render(<Greet />)
+        // const timeEl = screen.getByRole('heading', {name: 'Timeout Check'})
+        // If the El not found getBy method throws an error so for disapperance elements we use  queryBy method it returns null if el not found
+        const timeEl = screen.queryByRole('heading', {name: 'Timeout Check'})
+        expect(timeEl).not.toBeInTheDocument()
+        const timeEl2 = await screen.findByRole('heading', {name: 'Timeout Check'}, {timeout:1300}) // default Timeout is 1 sec (1000 ms)
+        expect(timeEl2).toBeInTheDocument()
+    })
+    test('Check image is present or not', ()=> {
+        render(<Greet/>)
+        const imgEl = screen.getByAltText('image')
+        expect(imgEl).toBeInTheDocument()
     })
     test('After Click dismiss will be remove', ()=> {
         render(<Greet />)
@@ -28,3 +56,34 @@ describe('Greet Component', ()=> {
     })
 })
 
+        // Query Methods :
+        /* 
+        getBy & getAllBy  --> for Appearance Elements
+        queryBy & queryAllBy --> for disapperance Elements
+        findBy & findAllBy --> Its combination of getBy and WaitFor queries supports async funcationality
+        */
+
+        // Queries 
+
+        /*
+        1.getByRole
+        2.getByLabelText
+        3.getByPlaceholderText
+        4.getByText
+        5.getByDisplayValue
+        6.getByAltText
+        7.getByTitle
+        8.getByTestid
+
+        */
+
+        /* 
+        // getByRole 
+        => getByRole accepts two arguments one is role, and second is optional that maybe name, level, checked, hidden, pressed
+
+        */
+
+        /*
+        // getByLabelText
+        => getByLabelText is our second priority it also accepts two arguments, one is label name, and second one is optional that will be the selector , it accepts html tag to find the element
+        */
